545042cb0e4f0e0d793e83a9951ba66bc2ae033a
Kolapo Adedipe <kolapoadedipe36@gmail.com>
2023-05-13 07:07:30 -0700

    Updated

--- console.py
+++ console.py
@@ -41,7 +41,8 @@ if __name__ == '__main__':
     HBNBCommand().cmdloop()
 
 
-
+#!/usr/bin/python3
+""" Entry point of the command interpreter """
 import cmd
 from models import storage
 from models.base_model import BaseModel
